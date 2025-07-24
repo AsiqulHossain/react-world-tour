088a6b20551eb10983e8b6feae7897bc6c30ce74
AsiqulHossain <ashiqulhossain@gmail.com>
2025-07-25 01:05:51 +0600

    add area and population

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Country.css'
 
 const Country = ({country}) => {
-    const{name, flags} =country;
+    const{name, flags,population,area} =country;
     return (
         <div className='country'>
             
             <h3>Name:{name?.common}</h3>
            <img src={flags.png} alt="" />
+           <p>Population:{population}</p>
+           <p>area: {area}</p>
         </div>
     );
 };
